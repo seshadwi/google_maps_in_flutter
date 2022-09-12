eb6cc73b3fa7f9e0e2037c1e152a4a528cdad0f1
seshadwi <seshaadwi@gamil.com>
2022-09-12 16:27:20 +0700

    add marker to location

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'src/locations.dart' as locations;
 
-void main() {
-  runApp(const MyApp());
-}
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:geocoder/geocoder.dart';
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -14,22 +14,46 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final Map<String, Marker> _markers = {};
-  Future<void> _onMapCreated(GoogleMapController controller) async {
-    final googleOffices = await locations.getGoogleOffices();
+  late GoogleMapController mapController;
+
+  static const double _defaultLat = -6.3439167;
+  static const double _defaultLng = 106.8866944;
+
+  // final LatLng _center = const LatLng(-6.3439167, 106.8866944);
+
+  static const CameraPosition _defaultLocation =
+      CameraPosition(target: LatLng(_defaultLat, _defaultLng), zoom: 15);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  MapType _currentMapType = MapType.normal;
+  final Set<Marker> _markers = {};
+
+  void _changeMapType() {
     setState(() {
-      _markers.clear();
-      for (final office in googleOffices.offices) {
-        final marker = Marker(
-          markerId: MarkerId(office.name),
-          position: LatLng(office.lat, office.lng),
+      _currentMapType = _currentMapType == MapType.normal
+          ? MapType.satellite
+          : MapType.normal;
+    });
+  }
+
+  void _addMarker() async {
+    var addresses = await Geocoder.local
+        .findAddressesFromCoordinates(Coordinates(_defaultLat, _defaultLng));
+    setState(() {
+      _markers.add(
+        Marker(
+          markerId: MarkerId('defaultLocation'),
+          position: _defaultLocation.target,
+          icon: BitmapDescriptor.defaultMarker,
           infoWindow: InfoWindow(
-            title: office.name,
-            snippet: office.address,
+            title: "${addresses.first.featureName}",
+            snippet: "${addresses.first.addressLine}",
           ),
-        );
-        _markers[office.name] = marker;
-      }
+        ),
+      );
     });
   }
 
@@ -39,18 +63,105 @@ class _MyAppState extends State<MyApp> {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Google Office Locations'),
+          title: const Text('Maps App'),
           backgroundColor: Colors.green[700],
         ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: const CameraPosition(
-            target: LatLng(0, 0),
-            zoom: 2,
-          ),
-          markers: _markers.values.toSet(),
+        body: Stack(
+          children: <Widget>[
+            GoogleMap(
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: _defaultLocation,
+              mapType: _currentMapType,
+              markers: _markers,
+              // CameraPosition(
+              // target: _center,
+              // zoom: 11.0,
+              // ),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 24, right: 12),
+              alignment: Alignment.topRight,
+              child: Column(
+                children: <Widget>[
+                  FloatingActionButton(
+                    onPressed: _changeMapType,
+                    backgroundColor: Colors.green,
+                    child: const Icon(
+                      Icons.map,
+                      size: 30,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  FloatingActionButton(
+                    onPressed: _addMarker,
+                    backgroundColor: Colors.deepPurpleAccent,
+                    child: const Icon(
+                      Icons.location_pin,
+                      size: 30,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
+// import 'package:flutter/material.dart';
+// import 'package:google_maps_flutter/google_maps_flutter.dart';
+// import 'src/locations.dart' as locations;
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatefulWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   _MyAppState createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   final Map<String, Marker> _markers = {};
+//   Future<void> _onMapCreated(GoogleMapController controller) async {
+//     final googleOffices = await locations.getGoogleOffices();
+//     setState(() {
+//       _markers.clear();
+//       for (final office in googleOffices.offices) {
+//         final marker = Marker(
+//           markerId: MarkerId(office.name),
+//           position: LatLng(office.lat, office.lng),
+//           infoWindow: InfoWindow(
+//             title: office.name,
+//             snippet: office.address,
+//           ),
+//         );
+//         _markers[office.name] = marker;
+//       }
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Google Office Locations'),
+//           backgroundColor: Colors.green[700],
+//         ),
+//         body: GoogleMap(
+//           onMapCreated: _onMapCreated,
+//           initialCameraPosition: const CameraPosition(
+//             target: LatLng(0, 0),
+//             zoom: 2,
+//           ),
+//           markers: _markers.values.toSet(),
+//         ),
+//       ),
+//     );
+//   }
+// }
