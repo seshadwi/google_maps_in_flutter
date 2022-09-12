460897a122b1bc348e28fc2fc0d8c2253c5ad9e1
seshadwi <seshaadwi@gamil.com>
2022-09-12 12:08:52 +0700

    show maps to screen

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +8,11 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Add Google Maps API Key
+    GMSServices.provideAPIKey("AIzaSyBXgp7mOQ6cBaFrptdz0Tld7M30O4IxJyE")
+    
     GeneratedPluginRegistrant.register(with: self)
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
